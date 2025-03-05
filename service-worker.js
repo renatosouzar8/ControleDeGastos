f35f25d59eb2c223fbdea10c8022ae377f51352a
renatosouzar8 <renator8@yahoo.com.br>
2025-03-05 12:15:18 -0300

    Primeiro upload do Controle de Gastos

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker instalado");
+  event.waitUntil(
+    caches.open("app-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
